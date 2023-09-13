e792ea53da7e0185d850e89b48e42d208edfa497
cameron oakes <cameronoakes@outlook.com>
2023-09-13 12:23:18 +0100

    home page and controller changes

--- test/controllers/mapple_games_controller_test.rb
+++ test/controllers/mapple_games_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class MappleGameControllerTest < ActionDispatch::IntegrationTest
+class MappleGamesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
