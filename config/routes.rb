14c1594d2d17cef346696873ca7fbb6e779f6a33
nicole mcinerney <nicolemci@hotmail.com>
2023-09-19 04:34:42 +0100

    basic user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/profile'
   # Defines the root path route ("/")
   # root "articles#index"
   devise_for :users
   root to: "mapple_games#home"
   get 'guest', to: 'pages#guest', as: 'your_guest'
+  get 'profile', to: 'users#profile', as: :user_profile
 
   resources :mapple_games
 end
