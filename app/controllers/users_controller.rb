5eca6e9422e7f40b510da49ca66eafd4f2d871c8
Cameron <112937913+CameronOakes@users.noreply.github.com>
2023-09-21 18:30:33 +0100

    Revert "Profile page"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,34 +3,5 @@ class UsersController < ApplicationController
 
   def profile
     @user = current_user
-    @total_games_played = calculate_total_games_played(@user)
-    # @games_won = calculate_games_won(@user)
-    @average_score = calculate_average_score(@user)
-    @total_score = calculate_total_score(@user)
-  end
-
-  private
-
-  def calculate_total_games_played(user)
-    user.mapple_games.count
-  end
-
-  # def calculate_games_won(user)
-    # user.mapple_games.where(won: true).count
-  # end
-
-  def calculate_average_score(user)
-    total_score = user.mapple_games.sum(:score)
-    total_games_played = user.mapple_games.count.to_f
-
-    if total_games_played > 0
-      (total_score / total_games_played).round(2)
-    else
-      0
-    end
-  end
-
-  def calculate_total_score(user)
-    user.mapple_games.sum(:score)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # Define the root path route ("/")
-  root "mapple_games#home"
-
+  get 'users/profile'
+  # Defines the root path route ("/")
+  # root "articles#index"
   devise_for :users
-
+  root to: "mapple_games#home"
   get 'guest', to: 'pages#guest', as: 'your_guest'
   get 'profile', to: 'users#profile', as: :user_profile
 
